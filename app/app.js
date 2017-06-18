6890416b784b0c8fb4cd6f246bb6f2446f8a83c8
ajay narain mathur <ajaynarainmathur@gmail.com>
2017-06-18 20:43:56 +1000

    initial timer added

--- app/app.js
+++ app/app.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import Timer from './components/timer';
 
-ReactDOM.render(<Timer/>, document.getElementById('root'));
+ReactDOM.render(<Timer endDate={'Sun Jun 25 2017 18:12:53 GMT+1000 (AEST)'} />, document.getElementById('root'));
